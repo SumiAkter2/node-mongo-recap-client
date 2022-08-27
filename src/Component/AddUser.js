310fc05ff5c8f80c124109f911538bc62425c952
SumiAkter2 <akteralisha36@gmail.com>
2022-08-27 09:10:07 +0600

    fetch with post method

--- src/Component/AddUser.js
+++ src/Component/AddUser.js
@@ -6,17 +6,28 @@ const AddUser = () => {
     const name = e.target.name.value;
     const email = e.target.email.value;
     const user = { name, email };
-    console.log("added", user);
+    console.log(user);
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
   return (
     <div>
-      <h1> Please fill the Input </h1>
+      <h1>Add a user</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Name" required />
+        <input type="text" name="name" placeholder="name" required />
         <br />
-        <input type="text" name="email" placeholder="Email" required />
+        <input type="text" name="email" placeholder="email" required />
         <br />
-        <button type="submit">Add a user</button>
+        <button type="submit">Added user</button>
       </form>
     </div>
   );
